be7fa3b46b8ea28097a2c87e177c1c208346ecf0
K5555kanafgo <jinjingshouta09@gmail.com>
2023-11-25 17:16:49 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
-  get root to: "messages#index"
-  get 'users', to: 'users#index'
+  root to: "messages#index"
+  resources :users, only: [:edit, :update]
   # Defines the root path route ("/")
   # root "articles#index"
 end
